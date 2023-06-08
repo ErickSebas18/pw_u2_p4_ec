d9fe992c51dba253c4a0b4b735067989905fec87
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-07 19:42:19 -0500

    Taller13

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("Componente Vue")
+console.log(Vue)
+
+const app = Vue.createApp({
+
+    //Opcion API
+    //opcion1,
+    //opcion2,
+    //opcion3
+
+    template: `
+    <h1>HOLA MUNDO </h1>
+    <p> Desde Vue.js`
+    
+}) 
+
+
+app.mount('#app')
