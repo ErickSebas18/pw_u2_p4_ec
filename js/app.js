beb149c36cdef736e658b426fb207f5a490cd260
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:42:49 -0500

    Taller14

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
-console.log("Componente Vue")
-console.log(Vue)
+console.log("Componente Vue");
+console.log(Vue);
 
 const app = Vue.createApp({
+  //Opcion API
+  //opcion1,
+  //opcion2,
+  //opcion3
 
-    //Opcion API
-    //opcion1,
-    //opcion2,
-    //opcion3
-
-    template: `
+  /*template: `
     <h1>HOLA MUNDO </h1>
-    <p> Desde Vue.js`
-    
-}) 
+    <p> Desde Vue.js </p>`*/
 
+  /*opcion del optionAPI que me permite declarar propiedades reactivas*/
+  /*me permiten vincular información de la vista con mi programación javascript*/
+  data() {
+    return {
+      nombre: "Erick",
+      apellido: "Chávez",
+      mensaje: "Hola Mundo 2 desde VUE.JS",
+    };
+  },
 
-app.mount('#app')
+  methods: {
+    cambiarNombre() {
+      console.log("Boton Cambiar Nombre");
+      this.nombre = "Sebastián"
+    },
+
+    cambiarApellido() {
+      console.log("Boton Cambiar Nombre");
+      this.apellido = "García"
+    },
+  },
+});
+
+app.mount("#app");
